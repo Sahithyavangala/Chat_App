1fe7c663780bbc47b492f0ad846076250f672991
Sahithyavangala <sahithyavangala86@gmail.com>
2025-04-06 14:10:02 +0530

    completed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,3 +4,4 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chat-app-3-o9up.onrender.com/api",
   withCredentials: true,
 });
+
